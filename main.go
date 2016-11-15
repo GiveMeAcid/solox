c690ff53c3ac6adae2798b8b84acbbb1802d09ac
Arctur <mazutichek@gmail.com>
2016-11-15 10:35:14 +0200

    added indexes

--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import (
 	_ "database/sql"
 	"fmt"
-	sc "solox/data_access/structs/conversations"
-	se "solox/data_access/structs/events"
-	su "solox/data_access/structs/users"
+	conv "solox/data_access/structs/conversations"
+	evnt "solox/data_access/structs/events"
+	user "solox/data_access/structs/users"
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
@@ -17,21 +17,21 @@ func main() {
 
 	if err != nil {
 		fmt.Printf("Database opening error -->%v\n", err)
-		//panic("Database error")
+		panic("Database error")
 	}
 	defer db.Close()
 
-	db1 := db.AutoMigrate(&su.UserSettings{}, &su.UserFilters{}, &sc.Conversation{}, &sc.Messages{}, &sc.UserConversation{}, &su.User{},
-		se.Events{}, se.UserEvent{})
+	db1 := db.AutoMigrate(&user.UserSettings{}, &user.UserFilters{}, &conv.Conversation{}, &conv.Messages{}, &conv.UserConversation{}, &user.User{},
+		evnt.Events{}, evnt.UserEvent{})
 
-	db.Model(&sc.Conversation{}).AddIndex("conversation_index", "conversation_id")
-	db.Model(&se.Events{}).AddIndex("event_index", "event_id")
-	db.Model(&sc.Messages{}).AddIndex("message_index", "conversation_fk")
-	db.Model(&sc.UserConversation{}).AddIndex("user_conversation_index", "conversation_id")
-	db.Model(&se.UserEvent{}).AddIndex("user_event_index", "event_id")
-	db.Model(&su.UserFilters{}).AddIndex("user_filters_index", "user_filters_id")
-	db.Model(&su.User{}).AddIndex("user_index", "user_id")
-	db.Model(&su.UserSettings{}).AddIndex("user_settings_index", "user_settings_id")
+	db.Model(&conv.Conversation{}).AddIndex("conversation_index", "conversation_id")
+	db.Model(&evnt.Events{}).AddIndex("event_index", "event_id")
+	db.Model(&conv.Messages{}).AddIndex("message_index", "conversation_fk")
+	db.Model(&conv.UserConversation{}).AddIndex("user_conversation_index", "conversation_id")
+	db.Model(&evnt.UserEvent{}).AddIndex("user_event_index", "event_id")
+	db.Model(&user.UserFilters{}).AddIndex("user_filters_index", "user_filters_id")
+	db.Model(&user.User{}).AddIndex("user_index", "user_id")
+	db.Model(&user.UserSettings{}).AddIndex("user_settings_index", "user_settings_id")
 
 	if db1.Error != nil {
 		panic(db1.Error)
